50ee99fabc577dd31c0cea2c25c3f28fda862106
LiXiaolong <562898064@qq.com>
2022-07-26 14:01:06 +0800

    临时提交，测试unsigned char *

--- tests/test.py
+++ tests/test.py
@@ -6,9 +6,10 @@
 # this software and related documentation outside the terms of the EULA
 # is strictly prohibited.
 import ctypes
-import math
+
 import numpy as np
 from cuda import cuda, cudart
+
 from core.helper import common
 from core.helper.helper_cuda import checkCudaErrors, findCudaDeviceDRV
 
@@ -19,28 +20,26 @@ typedef struct {
 } VOI;
 
 __constant__ float const_transform_matrix[9];
-cudaTextureObject_t volume_text_obj;
 cudaArray* d_volume_array = 0;
-cudaExtent volume_size;
 
 unsigned char* p_vr = 0;
-float3 normal, spacing, max_per;
+float3 normal;
 int volume_of_interest[6];
 VOI voi;
 int width_vr = 0;
 
 // Device code
-extern "C" __global__ void VecAdd_kernel(const float *A, const float *B, float *C, int N)
+extern "C" __global__ void TestCharPtr(unsigned char* pPixelData, int N)
 {
     int i = blockDim.x * blockIdx.x + threadIdx.x;
 
     if (i < N)
-        C[i] = A[i] + B[i];
+        pPixelData[i] = i % 127;
 }
 '''
 
 
-def test_modify_const(kernel_helper):
+def test_const(kernel_helper):
     transform_matrix = np.identity(3).astype(dtype=np.float32)
     d_transform_matrix, d_transform_matrix_size = kernel_helper.getGlobal(b'const_transform_matrix')
 
@@ -54,7 +53,7 @@ def test_modify_const(kernel_helper):
     ))
 
 
-def test_modify_intarr(kernel_helper):
+def test_intarr(kernel_helper):
     h_array = np.random.randint(0, 9, size=6).astype(dtype=np.int32)
     d_ptr, d_size = kernel_helper.getGlobal(b'volume_of_interest')
 
@@ -70,7 +69,7 @@ def test_modify_intarr(kernel_helper):
     assert (h_array == h_array2).all()
 
 
-def test_modify_int(kernel_helper):
+def test_int(kernel_helper):
     h_width = ctypes.c_float(2)
     d_ptr, d_size = kernel_helper.getGlobal(b'width_vr')
 
@@ -86,7 +85,7 @@ def test_modify_int(kernel_helper):
     assert h_width.value == h_width2.value
 
 
-def test_modify_float3(kernel_helper):
+def test_float3(kernel_helper):
     h_array = np.random.rand(3).astype(dtype=np.float32)
     d_ptr, d_size = kernel_helper.getGlobal(b'normal')
 
@@ -137,83 +136,57 @@ def test_cuda_extent(kernel_helper):
     extent.depth = 0
 
 
+def test_char_ptr(kernel_helper):
+    N = 512 * 512
+
+    buffer_size = np.dtype(np.int8).itemsize * N
+    dbuf = checkCudaErrors(cuda.cuMemAlloc(buffer_size))
+
+    _TestCharPtr = kernel_helper.getFunction(b'TestCharPtr')
+    threads_per_block = 256
+    blocks_per_grid = (N + threads_per_block - 1) / threads_per_block
+
+    kernel_args = ((dbuf, N), (None, ctypes.c_int))
+    checkCudaErrors(cuda.cuLaunchKernel(
+        _TestCharPtr,
+        blocks_per_grid, 1, 1,
+        threads_per_block, 1, 1,
+        0, 0,
+        kernel_args, 0
+    ))
+
+    buf = np.empty(N, dtype='int8')
+    checkCudaErrors(cuda.cuMemcpyDtoH(buf, dbuf, np.dtype(np.int8).itemsize * N))
+    pass
+
+
 def test_cudaArray(kernel_helper):
     pass
 
 
 def main():
     print("Vector Addition (Driver API)")
-    N = 50000
-    devID = 0
-    size = N * np.dtype(np.float32).itemsize
-
     # Initialize
-    checkCudaErrors(cuda.cuInit(0));
+    checkCudaErrors(cuda.cuInit(0))
 
-    cuDevice = findCudaDeviceDRV()
-    # Create context
-    cuContext = checkCudaErrors(cuda.cuCtxCreate(0, cuDevice))
-
-    uvaSupported = checkCudaErrors(
-        cuda.cuDeviceGetAttribute(cuda.CUdevice_attribute.CU_DEVICE_ATTRIBUTE_UNIFIED_ADDRESSING, cuDevice))
-    if not uvaSupported:
+    cu_device = findCudaDeviceDRV()
+    uva_supported = checkCudaErrors(
+        cuda.cuDeviceGetAttribute(cuda.CUdevice_attribute.CU_DEVICE_ATTRIBUTE_UNIFIED_ADDRESSING, cu_device))
+    if not uva_supported:
         print("Accessing pageable memory directly requires UVA")
         return
 
-    kernelHelper = common.KernelHelper(vectorAddDrv, int(cuDevice))
-    _VecAdd_kernel = kernelHelper.getFunction(b'VecAdd_kernel')
+    kernel_helper = common.KernelHelper(vectorAddDrv, int(cu_device))
 
-    # Allocate input vectors h_A and h_B in host memory
-    h_A = np.random.rand(size).astype(dtype=np.float32)
-    h_B = np.random.rand(size).astype(dtype=np.float32)
-    h_C = np.random.rand(size).astype(dtype=np.float32)
+    test_const(kernel_helper)
+    test_intarr(kernel_helper)
+    test_struct(kernel_helper)
+    test_int(kernel_helper)
+    test_float3(kernel_helper)
+    test_cuda_extent(kernel_helper)
+    test_char_ptr(kernel_helper)
 
-    # Allocate vectors in device memory
-    d_A = checkCudaErrors(cuda.cuMemAlloc(size))
-    d_B = checkCudaErrors(cuda.cuMemAlloc(size))
-    d_C = checkCudaErrors(cuda.cuMemAlloc(size))
-
-    # Copy vectors from host memory to device memory
-    checkCudaErrors(cuda.cuMemcpyHtoD(d_A, h_A, size))
-    checkCudaErrors(cuda.cuMemcpyHtoD(d_B, h_B, size))
-
-    # Grid/Block configuration
-    threadsPerBlock = 256
-    blocksPerGrid = (N + threadsPerBlock - 1) / threadsPerBlock
-
-    kernelArgs = ((d_A, d_B, d_C, N),
-                  (None, None, None, ctypes.c_int))
-
-    # Launch the CUDA kernel
-    checkCudaErrors(cuda.cuLaunchKernel(_VecAdd_kernel,
-                                        blocksPerGrid, 1, 1,
-                                        threadsPerBlock, 1, 1,
-                                        0, 0,
-                                        kernelArgs, 0))
-
-    # Copy result from device memory to host memory
-    # h_C contains the result in host memory
-    checkCudaErrors(cuda.cuMemcpyDtoH(h_C, d_C, size))
-
-    for i in range(N):
-        sum_all = h_A[i] + h_B[i]
-        if math.fabs(h_C[i] - sum_all) > 1e-7:
-            break
-
-    # Free device memory
-    checkCudaErrors(cuda.cuMemFree(d_A))
-    checkCudaErrors(cuda.cuMemFree(d_B))
-    checkCudaErrors(cuda.cuMemFree(d_C))
-
-    checkCudaErrors(cuda.cuCtxDestroy(cuContext))
-    print("{}".format("Result = PASS" if i + 1 == N else "Result = FAIL"))
-
-    test_modify_const(kernelHelper)
-    test_modify_intarr(kernelHelper)
-    test_struct(kernelHelper)
-    test_modify_int(kernelHelper)
-    test_modify_float3(kernelHelper)
-    test_cuda_extent(kernelHelper)
+    print('Test Finished')
 
 
 if __name__ == "__main__":
